d22ed7eb58fec05bccdf293e20b38ce741d4a7b3
shgadwal <sheetal.gadwal@gmail.com>
2024-01-10 00:45:10 -0500

    add test

---
+++ src/test.java
@@ -0,0 +1,12 @@
+
+public class test {
+
+	public static void main(String[] args) {
+	 String myfirstname = "sheetal ";
+	 String mylast = "gadwal";
+	 
+	 System.out.println(" print my name : "+ myfirstname + mylast);
+
+	}
+
+}
